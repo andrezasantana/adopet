0cad725518a5aabf6c7d7ddae8f0f07403ad3119
Andreza Santana <andreza.santana16@gmail.com>
2021-07-07 15:30:41 -0300

    create author component

---
+++ src/components/author/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Img from '../../assets/author.jpg'
+import './style.css';
+
+const Author = () => {
+    return (
+        <div className="author">
+            <img src={Img} alt="foto da autora da página"></img>
+            <div className="bio">
+                <h2>Andreza Santana</h2>
+                <p>Sou daquelas que gosta de viver e aproveitar as pequenas coisas da vida, adoro um bom café em uma dia chuvoso. Me chamo Andreza mas para os amigos é Deza.</p>
+                <div>
+                    <a href="https://www.instagram.com/deza.santanna/" target="_blank">Instagram |</a>
+                    <a href="https://www.linkedin.com/in/andreza-santana-7a4009a2/" target="_blank">Linkedin|</a>
+                    <a href="https://github.com/andrezasantana" target="_blank">GitHub</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Author;
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Menu from "../../components/menu";
 import Footer from "../../components/footer";
+import Author from "../../components/author"
 
 
 const About = () => {
     return (
         <>
             <Menu />
+            <Author />
             <Footer />
         </>
     )
