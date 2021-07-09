1426cabe6b14a8bc517df21c7593cd4d4f2948cb
Andreza Santana <andreza.santana16@gmail.com>
2021-07-09 20:39:54 -0300

    actualize author component

--- src/components/author/index.js
+++ src/components/author/index.js
@@ -7,8 +7,10 @@ const Author = () => {
         <div className="author">
             <img src={Img} alt="foto da autora da página"></img>
             <div className="bio">
-                <h2>Andreza Santana</h2>
-                <p>Sou daquelas que gosta de viver e aproveitar as pequenas coisas da vida, adoro um bom café em uma dia chuvoso. Me chamo Andreza mas para os amigos é Deza.</p>
+                <h2>O Projeto</h2>
+                <p>AdoPet é um projeto de conclusão do curso de Front-end da Reprograma! Foi criado com o objetivo de aproximar as pessoas que querem adotar um pet e os peludos de quatro patas mais fofos do mundo. Buscando incentivar a adoção, e desestimular o mercado de venda de animais. Afinal amigo não se compra!</p>
+                <h2>A Autora</h2>
+                <p>Meu nome é Andreza Santana. Sou daquelas que gosta de viver e aproveitar as pequenas coisas da vida. Desenvolvedora Front-end formada pela Reprograma.</p>
                 <div>
                     <a href="https://www.instagram.com/deza.santanna/" target="_blank" rel="noreferrer">Instagram |</a>
                     <a href="https://www.linkedin.com/in/andreza-santana-7a4009a2/" target="_blank" rel="noreferrer">Linkedin|</a>
